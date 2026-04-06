89901e165abfa45b04516353eb62c5bf5590651a
Arturo Carrillo Jimenez <acarrilloj05@gmail.com>
2026-04-06 17:42:16 +0200

    feat: initialize distributed scraping project structure with FastAPI producer, SQS infrastructure, and shared data models

--- infra/init-aws.sh
+++ infra/init-aws.sh
@@ -1,4 +1,9 @@
-#!/bin/bash
-awslocal sqs create-queue --queue-name scraping-tasks-queue
-awslocal s3 mb s3://raw-data-tfm
-echo "Infrastructure initialized: SQS queue and S3 bucket created."
+#!/bin/sh
+echo "----------- Initializing Infrastructure -----------"
+
+# Crear la cola SQS
+awslocal  sqs create-queue \
+ --queue-name message-to-sqs.fifo \
+ --attributes FifoQueue=true
+
+ echo "----------- Initializing Ready -----------"
--- producer/main.py
+++ producer/main.py
@@ -1,6 +1,8 @@
-def main():
-    print("Hello from producer!")
+from fastapi import FastAPI
+from shared.models import ScrapingTask
 
+app = FastAPI(title='Web Scraping Distributed', version='1.0')
 
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__' :
+    import uvicorn
+    uvicorn.run(app = 'main:app', host='0.0.0.0', port=8000, reload = True)
