13e4c8582bb1b332724b76f92c579b259dde9cbe
Arturo Carrillo Jimenez <acarrilloj05@gmail.com>
2026-05-13 10:38:19 +0200

    feat: update settings to include default values for producer configuration and AWS credentials

--- worker/config/settings.py
+++ worker/config/settings.py
@@ -2,12 +2,14 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    num_max_tasks: int
-    default_region_aws: str
-    sqs_endpoint_url: str
-    sqs_queue_url: str
-    aws_access_key_id: str
-    aws_secret_access_key: str
+    num_max_tasks: int = 10
+    producer_port: int = 8000
+    producer_host: str = "localhost"
+    default_region_aws: str = "us-east-1"
+    sqs_endpoint_url: str = "http://localhost:9324"  # URL del endpoint de SQS local
+    sqs_queue_url: str = "http://localhost:9324/000000000000/my-queue"  # URL de la cola SQS local
+    aws_access_key_id: str = "test"  # Clave de acceso de AWS (puede ser cualquier valor para pruebas locales)
+    aws_secret_access_key: str = "test"  # Clave secreta de acceso de AWS (puede ser cualquier valor para pruebas locales)
 
     # Se usa model_config con SettingsConfigDict
     model_config = SettingsConfigDict(
